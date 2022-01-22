7a156abdaeef2aed19d00be94f67e39c0723ab4a
Joanna Swider <asia_tbg@tlen.pl>
2022-01-23 00:24:07 +0100

    Add capitalize

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ shopping_list= {
     "warzywniak": ["marchew", "seler", "rukola"]
   }
 print(shopping_list)
+for sklep, produkty in shopping_list.items():
+  print("Idę do", sklep ,", kupuję tu następujące rzeczy", [i for i in produkty])
+  
