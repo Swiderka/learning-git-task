bfe311c4eb097e11ea53c0876a0a35740677e5c1
Swiderka <98241198+Swiderka@users.noreply.github.com>
2022-01-24 11:48:47 +0100

    for Mentor

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ shopping_list= {
 print(shopping_list)
 for sklep, produkty in shopping_list.items():
   print("Idę do", sklep.capitalize(),", kupuję tu następujące rzeczy", [i.title()for i in produkty])
+print("Pozdrawiam mojego wytrwałego Mentora :) trudny Kursant trafił się :D ")
